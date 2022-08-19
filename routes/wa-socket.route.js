85dd96a94839b0adf1edded6fa2ba524a69a5de3
Rajat Sharma <rxx175@gmail.com>
2022-08-20 01:17:45 +0530

    added wa-socket routes

---
+++ routes/wa-socket.route.js
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import {
+   sendWASocketStatus,
+   requestToReopenWASocket,
+   requestToCloseWASocket,
+} from '../controllers/wa-socket.controller.js';
+
+const router = Router();
+
+router.get('/status', sendWASocketStatus);
+router.put('/open', requestToReopenWASocket);
+router.put('/close', requestToCloseWASocket);
+
+export default router;
