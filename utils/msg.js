5d37358c991060bdacaf0378ced7fb708c924457
Rajat Sharma <rajat@trivediat.com>
2022-08-25 00:09:15 +0530

    written msg utility

---
+++ utils/msg.js
@@ -0,0 +1,32 @@
+import { encode } from 'msgpackr';
+import { MsgPayloadTypes } from '../config/msg.js';
+
+/**
+ * Returns encoded presence update notification payload
+ * @param {string} alias
+ * @param {boolean} status
+ */
+export function encodePresenceUpdateNotificationPayload(alias, status) {
+   /** @type {PresenceNotificationPayload} */
+   const payload = [MsgPayloadTypes.PRESENCE_UPDATE, alias, status, Date.now()];
+   return encode(payload);
+}
+
+/**
+ * Returns encoded service status update notification payload
+ * @param {number} serviceType
+ * @param {boolean} status
+ */
+export function encodeServiceStatusUpdateNotificationPayload(
+   serviceType,
+   status
+) {
+   /** @type {ServiceStatusUpdateNotificationPayload} */
+   const payload = [
+      MsgPayloadTypes.SERVER_STATUS_UPDATE,
+      serviceType,
+      status,
+      Date.now(),
+   ];
+   return encode(payload);
+}
