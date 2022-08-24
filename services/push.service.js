8504f32f262170ab83fca2fdd35ac4623b4b011f
Rajat Sharma <rajat@trivediat.com>
2022-08-25 00:10:26 +0530

    update push service

--- services/push.service.js
+++ services/push.service.js
@@ -1,21 +1,41 @@
-import { getEnabledPushSubscriptions } from './push-subs.service.js';
+import { getPushSubscriptionPayloadList } from './push-subs.service.js';
 import { webpush } from '../utils/webpush.js';
+import {
+   encodePresenceUpdateNotificationPayload,
+   encodeServiceStatusUpdateNotificationPayload,
+} from '../utils/msg.js';
 
 /**
  * Sends presence update push notification
- * @param {Subscription} subscription 
- * @param {number} status 
+ * @param {Subscription} subscription
+ * @param {number} status
  */
 export async function sendPresenceUpdateNotification(subscription, status) {
-   const pushsubs = await getEnabledPushSubscriptions();
-   const psubsPromises = pushsubs.map(function(psubs) {
-      const subs = JSON.parse(String(psubs.payload));
-      return webpush()
-         .sendNotification(
-            subs, 
-            `${subscription.alias} is ${status === 1? 'online': 'offline'}`,
-         );
+   const payloadList = await getPushSubscriptionPayloadList();
+   const psubsPromises = payloadList.map(function (pushpayload) {
+      const data = encodePresenceUpdateNotificationPayload(
+         subscription.alias,
+         Boolean(status)
+      );
+      return webpush().sendNotification(pushpayload, data.toString('base64'));
    });
 
+   return Promise.all(psubsPromises);
+}
+
+/**
+ * Sends service update push notification
+ * @param {number} serviceType
+ * @param {boolean} status
+ */
+export async function sendServiceStatusUpdateNotification(serviceType, status) {
+   const payloadList = await getPushSubscriptionPayloadList();
+   const psubsPromises = payloadList.map(function (pushpayload) {
+      const data = encodeServiceStatusUpdateNotificationPayload(
+         serviceType,
+         status
+      );
+      return webpush().sendNotification(pushpayload, data.toString('base64'));
+   });
    return Promise.all(psubsPromises);
 }
