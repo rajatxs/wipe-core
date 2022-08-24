c501faf1978a0705dad1efcfc12fd7edbf530412
Rajat Sharma <rxx175@gmail.com>
2022-08-25 00:10:59 +0530

    added notification events

--- controllers/wa-socket.controller.js
+++ controllers/wa-socket.controller.js
@@ -1,5 +1,7 @@
 import { send200Response } from '../utils/http.js';
 import { waSocket, openWASocket, closeWASocket } from '../sockets/wa.socket.js';
+import { sendServiceStatusUpdateNotification } from '../services/push.service.js';
+import { MsgPayloadTypes } from '../config/msg.js';
 
 /**
  * Sends WA Socket status
@@ -24,6 +26,10 @@ export async function requestToReopenWASocket(req, res) {
    try {
       if (!waSocket()) {
          await openWASocket();
+         sendServiceStatusUpdateNotification(
+            MsgPayloadTypes.SOCKET_STATUS_UPDATE,
+            true
+         );
       }
       send200Response(res, 'Socket opened');
    } catch (error) {
@@ -39,6 +45,10 @@ export async function requestToReopenWASocket(req, res) {
 export function requestToCloseWASocket(req, res) {
    try {
       closeWASocket();
+      sendServiceStatusUpdateNotification(
+         MsgPayloadTypes.SOCKET_STATUS_UPDATE,
+         false
+      );
       send200Response(res, 'Socket closed');
    } catch (error) {
       throw new Error("Couldn't close socket connection");
