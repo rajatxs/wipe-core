2dfd4fb4bbfa93827f254872f4a8eca8a02b235c
Rajat Sharma <rxx175@gmail.com>
2022-08-28 13:04:17 +0530

    written sendPushSubscriptionCheck(), use next()

--- controllers/push-subs.controller.js
+++ controllers/push-subs.controller.js
@@ -5,6 +5,7 @@ import {
 } from '../utils/http.js';
 import {
    getAllPushSubscriptions,
+   checkPushSubscriptionBySha256,
    getPushSubscriptionById,
    createPushSubscription,
    deletePushSubscription,
@@ -14,15 +15,16 @@ import {
  * Send all push subscription records
  * @param {import('express').Request} req
  * @param {import('express').Response} res
+ * @param {import('express').NextFunction} next
  */
-export async function sendAllPushSubscriptions(req, res) {
+export async function sendAllPushSubscriptions(req, res, next) {
    const limit = Number(req.query.limit) || 10;
 
    try {
       const records = await getAllPushSubscriptions(limit);
       send200Response(res, "All push subscriptions", records);
    } catch (error) {
-      throw new Error("Couldn't get push subscriptions");
+      next(["Couldn't get push subscriptions", error]);
    }
 }
 
@@ -30,8 +32,9 @@ export async function sendAllPushSubscriptions(req, res) {
  * Send push subscription record by given id param
  * @param {import('express').Request} req
  * @param {import('express').Response} res
+ * @param {import('express').NextFunction} next
  */
-export async function sendPushSubscriptionById(req, res) {
+export async function sendPushSubscriptionById(req, res, next) {
    const id = Number(req.params.id);
 
    try {
@@ -43,16 +46,35 @@ export async function sendPushSubscriptionById(req, res) {
          send404Response(res, 'Push subscription not found');
       }
    } catch (error) {
-      throw new Error("Couldn't get push subscription");
+      next(["Couldn't get push subscription", error]);
    }
 }
+
+/**
+ * Send push subscription record check result by given `sha256` param
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
+ * @param {import('express').NextFunction} next
+ */
+export async function sendPushSubscriptionCheck(req, res, next) {
+   const sha256 = req.params.sha256;
+
+   try {
+      const exists = await checkPushSubscriptionBySha256(sha256);
+      send200Response(res, "Push subscription check", { exists });
+   } catch (error) {
+      next(["Couldn't check push subscription", error]);
+   }
+}
+
 
 /**
  * Add new push subscription record
  * @param {import('express').Request} req
  * @param {import('express').Response} res
+ * @param {import('express').NextFunction} next
  */
-export async function addNewPushSubscription(req, res) {
+export async function addNewPushSubscription(req, res, next) {
    /** @type {PushSubscriptionRecord} */
    const data = req.body;
    data.tag = req.locals.tag;
@@ -61,7 +83,7 @@ export async function addNewPushSubscription(req, res) {
       const result = await createPushSubscription(data);
       send201Response(res, 'Push subscription added', result);
    } catch (error) {
-      throw new Error("Couldn't add push subscription");
+      next(["Couldn't add push subscription", error]);
    }
 }
 
@@ -69,14 +91,15 @@ export async function addNewPushSubscription(req, res) {
  * Deletes push subscription record by given id param
  * @param {import('express').Request} req
  * @param {import('express').Response} res
+ * @param {import('express').NextFunction} next
  */
-export async function deleteSinglePushSubscription(req, res) {
+export async function deleteSinglePushSubscription(req, res, next) {
    const id = Number(req.params.id);
 
    try {
       const result = await deletePushSubscription(id);
       send200Response(res, 'Push subscription deleted', result);
    } catch (error) {
-      throw new Error("Couldn't delete push subscription");
+      next(["Couldn't delete push subscription", error]);
    }
 }
