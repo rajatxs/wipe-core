32e64aace0f4a6578376057265d6c3c524da38b5
Rajat Sharma <rxx256@gmail.com>
2024-03-31 00:19:35 +0530

    use STORE_ROOT config

--- config/config.js
+++ config/config.js
@@ -3,14 +3,18 @@ import { homedir } from 'os';
 import { getTag } from '../utils/common.js';
 
 // global vars
-export const NODE_ENV = process.env.NODE_ENV;
-export const PORT = process.env.PORT;
+export const NODE_ENV = process.env.NODE_ENV || 'development';
+export const PORT = process.env.PORT || '8000';
 
 // common vars
+export const STORE_ROOT = process.env.WIPE_STORE_ROOT || path.join(homedir(), '.wipe');
 export const TAG = process.env.WIPE_TAG || getTag();
 export const WEBPUSH_SUBJECT = process.env.WIPE_WEBPUSH_SUBJECT;
 export const AUTH_SECRET = process.env.WIPE_AUTH_SECRET;
-export const SESSION_ROOT = process.env.WIPE_SESSION_ROOT || path.join(homedir(), '.wipe');
+export const SESSION_ROOT = path.join(STORE_ROOT, 'session_v3');
+
+// SQLite config
+export const SQLITE_DB_FILE = path.join(STORE_ROOT, 'store_v3.db');
 
 // MySQL config
 export const MYSQL_HOST = process.env.WIPE_MYSQL_HOST;
