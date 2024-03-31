30115b3f40a16d3e58fba6d40adc16a428a41a67
Rajat Sharma <rxx256@gmail.com>
2024-03-31 17:36:05 +0530

    update subs test cases

--- services/subs.service.test.js
+++ services/subs.service.test.js
@@ -1,17 +1,17 @@
 import assert from 'assert';
-import { disconnect } from '../utils/mysql.js';
+import { openSQLiteDatabase, closeSQLiteDatabase } from '../utils/sqlite.js';
 import {
-   getSubscriptionById,
-   createSubscription,
-   updateSubscription,
-   deleteSubscription,
+    getSubscriptionById,
+    createSubscription,
+    updateSubscription,
+    deleteSubscription,
 } from './subs.service.js';
 
-const tag = "TEST";
+const tag = 'TEST';
 
 let subId,
-   alias = 'Test',
-   phone = '101010101010';
+    alias = 'Test',
+    phone = '101010101010';
 
 /** @type {SubscriptionEvent} */
 let event = 'presence.update';
@@ -20,51 +20,61 @@ let event = 'presence.update';
 const events = ['presence.update'];
 
 describe('Subscription service', function () {
-   this.afterAll(function (done) {
-      disconnect().then(done).catch(done);
-   });
+    this.beforeAll(function (done) {
+        openSQLiteDatabase().finally(done);
+    });
 
-   it('should insert new subscription record', async function () {
-      const res = await createSubscription({
-         alias,
-         event,
-         phone,
-         tag,
-      });
+    this.afterAll(function (done) {
+        closeSQLiteDatabase().finally(done);
+    });
 
-      assert.ok(res.insertId > 0, 'incorrect insertId');
-      assert.equal(res.affectedRows, 1, 'more rows are affected');
-      subId = res.insertId;
-   });
+    it('should insert new subscription record', async function () {
+        const insertId = await createSubscription({
+            alias,
+            event,
+            phone,
+            tag,
+        });
 
-   it('should returns subscription record', async function () {
-      const subs = await getSubscriptionById(subId);
+        assert.ok(insertId > 0, 'incorrect insertId');
+        subId = insertId;
+    });
 
-      assert.ok(subs, 'subscription not found');
-      assert.equal(subs.id, subId, 'incorrect subId');
-      assert.equal(typeof subs.enabled, 'number', 'invalid type of enabled');
-      assert.equal(typeof subs.alias, 'string', 'invalid type of alias');
-      assert.equal(subs.tag, tag, "incorrect tag");
-      assert.ok(events.includes(subs.event), 'incorrect event');
-      assert.equal(typeof subs.notify, 'number', 'invalid notify');
-      assert.equal(subs.phone.length, 12, 'incorrect phone');
-   });
+    it('should returns subscription record', async function () {
+        const subs = await getSubscriptionById(subId);
 
-   it('should update subscription record', async function () {
-      alias = 'New alias';
-      const res = await updateSubscription(subId, { alias });
-      assert.equal(res.affectedRows, 1, 'more rows are affected');
+        assert.ok(subs, 'subscription not found');
+        assert.equal(subs.id, subId, 'incorrect id');
+        assert.equal(typeof subs.enabled, 'number', 'invalid type of enabled');
+        assert.equal(typeof subs.alias, 'string', 'invalid type of alias');
+        assert.equal(subs.tag, tag, 'incorrect tag');
+        assert.ok(events.includes(subs.event), 'incorrect event');
+        assert.equal(typeof subs.notify, 'number', 'invalid notify');
+        assert.strictEqual(subs.phone, phone, 'incorrect phone');
+    });
 
-      const subs = await getSubscriptionById(subId);
-      assert.equal(subs.id, subId, 'subId is affected');
-      assert.equal(subs.alias, alias, 'alias not updated');
-   });
+    it('should update subscription record', async function () {
+        alias = 'New alias';
+        const changes = await updateSubscription(subId, { alias, enabled: 0, notify: 0 });
+        assert.equal(changes, 1, 'more rows are affected');
 
-   it('should delete subscription record', async function () {
-      const res = await deleteSubscription(subId);
-      assert.equal(res.affectedRows, 1, 'more rows are affected');
+        // Get updated subscription record
+        const subs = await getSubscriptionById(subId);
 
-      const subs = await getSubscriptionById(subId);
-      assert.ok(!subs, 'subscription record still exists');
-   });
+        assert.equal(subs.id, subId, 'id value has affected');
+        assert.equal(subs.tag, tag, 'tag value has tag');
+        assert.strictEqual(subs.phone, phone, 'phone value has affected');
+
+        assert.equal(subs.alias, alias, 'alias propety has not updated');
+        assert.equal(subs.enabled, 0, 'enabled property has not updated');
+        assert.equal(subs.notify, 0, 'notify property has not updated');
+    });
+
+    it('should delete subscription record', async function () {
+        const changes = await deleteSubscription(subId);
+        assert.equal(changes, 1, 'more rows are affected');
+
+        const subs = await getSubscriptionById(subId);
+        assert.ok(!subs, 'subscription record still exists');
+    });
 });
