5ff531de26283bbccf78091f2f4546c2ecc724c8
Rajat Sharma <rxx256@gmail.com>
2024-04-01 22:49:03 +0530

    format observer service code

--- services/observer.js
+++ services/observer.js
@@ -4,9 +4,7 @@ import { insertPresenceHistoryRecord } from './presence.js';
 import { sendPresenceUpdateNotification } from './push.js';
 import { TAG } from '../config/config.js';
 import { jidDecode } from '@whiskeysockets/baileys/lib/WABinary/jid-utils.js';
-import { format } from 'util';
 import { registerSocketEventBySubscription } from '../utils/wa-socket.js';
-import logger from '../utils/logger.js';
 
 /** @type {Map<string, number>} */
 const presenceUpdateCounts = new Map();
@@ -16,16 +14,16 @@ const presenceUpdateCounts = new Map();
  * @param {string} jid
  */
 export function incrementPresenceUpdateCount(jid) {
-   if (!presenceUpdateCounts.has(jid)) {
-      presenceUpdateCounts.set(jid, 0);
-   } else {
-      presenceUpdateCounts.set(jid, presenceUpdateCounts.get(jid) + 1);
-   }
+    if (!presenceUpdateCounts.has(jid)) {
+        presenceUpdateCounts.set(jid, 0);
+    } else {
+        presenceUpdateCounts.set(jid, presenceUpdateCounts.get(jid) + 1);
+    }
 }
 
 /** Removes all values from `presenceUpdateCounts`  */
 export function resetPresenceUpdateCounts() {
-   presenceUpdateCounts.clear();
+    presenceUpdateCounts.clear();
 }
 
 /**
@@ -34,60 +32,64 @@ export function resetPresenceUpdateCounts() {
  * @returns {Promise<any>}
  */
 export async function dispatchPresenceUpdateEvent(event) {
-   const jid = event.id;
-   let props = {},
-      status = 0,
-      lastseen = 0;
+    const jid = event.id;
+    let props = {},
+        status = 0,
+        lastseen = 0;
 
-   if (event.presences[jid] && typeof event.presences[jid] === 'object') {
-      props = event.presences[jid];
-   } else {
-      return Promise.resolve();
-   }
+    if (event.presences[jid] && typeof event.presences[jid] === 'object') {
+        props = event.presences[jid];
+    } else {
+        return Promise.resolve();
+    }
 
-   incrementPresenceUpdateCount(jid);
+    incrementPresenceUpdateCount(jid);
 
-   if ('lastKnownPresence' in props) {
-      // should track available status
-      if (props.lastKnownPresence === 'available') {
-         status = 1;
-      } else {
-         // prevent insertion of offline record
-         if (presenceUpdateCounts.get(jid) === 0) {
-            return Promise.resolve();
-         }
-      }
-   }
+    if ('lastKnownPresence' in props) {
+        // should track available status
+        if (props.lastKnownPresence === 'available') {
+            status = 1;
+        } else {
+            // prevent insertion of offline record
+            if (presenceUpdateCounts.get(jid) === 0) {
+                return Promise.resolve();
+            }
+        }
+    }
 
-   if ('lastSeen' in props && typeof props.lastSeen === 'number') {
-      lastseen = props.lastSeen;
-   }
+    if ('lastSeen' in props && typeof props.lastSeen === 'number') {
+        lastseen = props.lastSeen;
+    }
 
-   const { user: phone } = jidDecode(jid);
-   const subslist = await getSubscriptionByPhone('presence.update', phone);
+    const { user: phone } = jidDecode(jid);
+    const subslist = await getSubscriptionByPhone('presence.update', phone);
 
-   let subsPromises = subslist.map(async (subs) => {
-      if (subs.enabled === 1) {
-         const sub_id = subs.id;
-         const recordId = await insertPresenceHistoryRecord({
-            status,
-            lastseen,
-            sub_id,
-            tag: TAG,
-         });
+    let subsPromises = subslist.map(async (subs) => {
+        if (subs.enabled === 1) {
+            const sub_id = subs.id;
+            const recordId = await insertPresenceHistoryRecord({
+                status,
+                lastseen,
+                sub_id,
+                tag: TAG,
+            });
 
-         if (recordId > 0) {
-            debug('wipe:observer')('presence update sub_id=%d status=%d', sub_id, status);
-         }
+            if (recordId > 0) {
+                debug('wipe:observer')(
+                    'presence update sub_id=%d status=%d',
+                    sub_id,
+                    status
+                );
+            }
 
-         if (subs.notify === 1) {
-            return sendPresenceUpdateNotification(subs, status);
-         }
-      }
-      return Promise.resolve();
-   });
+            if (subs.notify === 1) {
+                return sendPresenceUpdateNotification(subs, status);
+            }
+        }
+        return Promise.resolve();
+    });
 
-   return Promise.all(subsPromises);
+    return Promise.all(subsPromises);
 }
 
 /**
@@ -95,11 +97,11 @@ export async function dispatchPresenceUpdateEvent(event) {
  * @returns {Promise<any>}
  */
 export async function registerPresenceUpdateEvent() {
-   const subslist = await getSubscriptions('presence.update', 5);
-   let subsPromises = subslist.map(async (subs) => {
-      await registerSocketEventBySubscription(subs);
-      debug('wipe:observer')('subscribe event=%s id=%d', subs.event, subs.id);
-   });
+    const subslist = await getSubscriptions('presence.update', 5);
+    let subsPromises = subslist.map(async (subs) => {
+        await registerSocketEventBySubscription(subs);
+        debug('wipe:observer')('subscribe event=%s id=%d', subs.event, subs.id);
+    });
 
-   return Promise.all(subsPromises);
+    return Promise.all(subsPromises);
 }
