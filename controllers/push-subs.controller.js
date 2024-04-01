4accf1c27627ceef41b56960f687af8813bbc882
Rajat Sharma <rxx256@gmail.com>
2024-04-01 15:33:34 +0530

    update push-subs controller methods

--- controllers/push-subs.controller.js
+++ controllers/push-subs.controller.js
@@ -1,13 +1,15 @@
+import debug from 'debug';
 import {
-   send200Response,
-   send201Response,
-   send404Response,
+    send200Response,
+    send201Response,
+    send404Response,
+    send500Response,
 } from '../utils/http.js';
 import {
-   getAllPushSubscriptions,
-   getPushSubscriptionById,
-   createPushSubscription,
-   deletePushSubscription,
+    getAllPushSubscriptions,
+    getPushSubscriptionById,
+    createPushSubscription,
+    deletePushSubscription,
 } from '../services/push-subs.service.js';
 
 /**
@@ -16,14 +18,15 @@ import {
  * @param {import('express').Response} res
  */
 export async function sendAllPushSubscriptions(req, res) {
-   const limit = Number(req.query.limit) || 10;
+    const limit = Number(req.query.limit) || 10;
 
-   try {
-      const records = await getAllPushSubscriptions(limit);
-      send200Response(res, "All push subscriptions", records);
-   } catch (error) {
-      throw new Error("Couldn't get push subscriptions");
-   }
+    try {
+        const records = await getAllPushSubscriptions(limit);
+        send200Response(res, 'All push subscriptions', records);
+    } catch (error) {
+        debug('wipe:controller:push-subs:error')(error);
+        send500Response(res, "Couldn't get push subscriptions");
+    }
 }
 
 /**
@@ -32,19 +35,20 @@ export async function sendAllPushSubscriptions(req, res) {
  * @param {import('express').Response} res
  */
 export async function sendPushSubscriptionById(req, res) {
-   const id = Number(req.params.id);
+    const id = Number(req.params.id);
 
-   try {
-      const record = await getPushSubscriptionById(id);
+    try {
+        const record = await getPushSubscriptionById(id);
 
-      if (record) {
-         send200Response(res, 'Push subscription', record);
-      } else {
-         send404Response(res, 'Push subscription not found');
-      }
-   } catch (error) {
-      throw new Error("Couldn't get push subscription");
-   }
+        if (record) {
+            send200Response(res, 'Push subscription', record);
+        } else {
+            send404Response(res, 'Push subscription not found');
+        }
+    } catch (error) {
+        debug('wipe:controller:push-subs:error')(error);
+        send500Response(res, "Couldn't get push subscription");
+    }
 }
 
 /**
@@ -53,16 +57,17 @@ export async function sendPushSubscriptionById(req, res) {
  * @param {import('express').Response} res
  */
 export async function addNewPushSubscription(req, res) {
-   /** @type {PushSubscriptionRecord} */
-   const data = req.body;
-   data.tag = req.locals.tag;
+    /** @type {PushSubscriptionRecord} */
+    const data = req.body;
+    data.tag = req.locals.tag;
 
-   try {
-      const result = await createPushSubscription(data);
-      send201Response(res, 'Push subscription added', result);
-   } catch (error) {
-      throw new Error("Couldn't add push subscription");
-   }
+    try {
+        const id = await createPushSubscription(data);
+        send201Response(res, 'Push subscription added', { id });
+    } catch (error) {
+        debug('wipe:controller:push-subs:error')(error);
+        send500Response(res, "Couldn't add push subscription");
+    }
 }
 
 /**
@@ -71,12 +76,13 @@ export async function addNewPushSubscription(req, res) {
  * @param {import('express').Response} res
  */
 export async function deleteSinglePushSubscription(req, res) {
-   const id = Number(req.params.id);
+    const id = Number(req.params.id);
 
-   try {
-      const result = await deletePushSubscription(id);
-      send200Response(res, 'Push subscription deleted', result);
-   } catch (error) {
-      throw new Error("Couldn't delete push subscription");
-   }
+    try {
+        const changes = await deletePushSubscription(id);
+        send200Response(res, 'Push subscription deleted', { changes });
+    } catch (error) {
+        debug('wipe:controller:push-subs:error')(error);
+        send500Response(res, "Couldn't delete push subscription");
+    }
 }
