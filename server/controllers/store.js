ef2f45f53728645b807bd100ee75e521c9771da2
Rajat Sharma <rxx256@gmail.com>
2024-04-04 17:23:16 +0530

    define downloadStoreFile controller

--- server/controllers/store.js
+++ server/controllers/store.js
@@ -2,7 +2,7 @@ import { createReadStream } from 'fs';
 import { stat } from 'fs/promises';
 import debug from 'debug';
 import { SQLITE_DB_FILE } from '../../config/config.js';
-import { send200Response, send500Response } from '../../utils/http.js';
+import { send200Response, send400Response, send500Response } from '../../utils/http.js';
 
 /**
  * Send store file info
@@ -37,5 +37,20 @@ export function sendStoreFile(req, res) {
     } catch (error) {
         debug('wipe:controller:store:error')(error.message);
         send500Response(res, "Couldn't stream store file");
+    }
+}
+
+/**
+ * Save new store file at default path
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
+ */
+export function downloadStoreFile(req, res) {
+    if (req.file) {
+        send200Response(res, 'File saved', {
+            path: req.file.path,
+        });
+    } else {
+        send400Response(res, 'File not provided');
     }
 }
--- server/routes/store.js
+++ server/routes/store.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
-import { sendStoreInfo, sendStoreFile } from '../controllers/store.js';
+import { sendStoreInfo, sendStoreFile, downloadStoreFile } from '../controllers/store.js';
+import { upload } from '../../utils/multer.js';
 
 const router = Router();
 
 router.get('/info', sendStoreInfo);
 router.get('/file', sendStoreFile);
+router.post('/file', upload.single('file'), downloadStoreFile);
 
 export default router;
