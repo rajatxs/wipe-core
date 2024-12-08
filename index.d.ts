0641781818f38ead9b6e178aaa67bab3e29fee0e
Rajat Sharma <rxx256@gmail.com>
2024-12-08 11:22:03 +0530

    remove Subscription type interface

--- index.d.ts
+++ index.d.ts
@@ -10,17 +10,6 @@ declare namespace Express {
 
 declare type SubscriptionEvent = 'presence.update';
 
-declare interface Subscription {
-    id?: number;
-    enabled?: number;
-    alias?: string;
-    event: SubscriptionEvent;
-    notify?: number;
-    phone: string;
-    tag?: string;
-    created_at?: string;
-}
-
 declare interface PresenceHistory {
     id?: number;
     status: number;
