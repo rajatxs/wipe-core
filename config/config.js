4dba06da3797e5909a81e05d588dca802d254b4e
Rajat Sharma <rxx256@gmail.com>
2025-01-03 23:16:16 +0530

    added `SUBSCRIBER_PHONE` config prop

--- config/config.js
+++ config/config.js
@@ -12,6 +12,7 @@ export const UPLOAD_ROOT = path.join(STORE_ROOT, 'uploads');
 export const TAG = process.env.WIPE_TAG || getTag();
 export const AUTH_SECRET = process.env.WIPE_AUTH_SECRET;
 export const SESSION_ROOT = path.join(STORE_ROOT, 'session_v3');
+export const SUBSCRIBER_PHONE = process.env.WIPE_SUBSCRIBER_PHONE;
 
 // SQLite config
 export const SQLITE_DB_FILE = path.join(STORE_ROOT, 'store_v3.db');
